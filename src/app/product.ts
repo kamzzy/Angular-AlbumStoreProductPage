3b74174e922aee9347da7cefe6de55d6a648a61c
kamzzy <preciousakams@yahoo.com>
2019-10-28 04:32:51 +0100

    .

---
+++ src/app/product.ts
@@ -0,0 +1,9 @@
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+export interface Product {
+    id: number;
+    artistName: string;
+    albumName: string;
+
+}
